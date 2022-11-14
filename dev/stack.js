2d85d10498f281f53afaa6a8447ee3db1a210c7b
Gaben4184 <grodfam4@gmail.com>
2022-11-14 12:29:16 -0700

    many lot changes

---
+++ dev/stack.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(1200, 1200);
+}
+  
+function draw() {
+    background(220);
+   
+    fill(20,200,30);
+    ellipse(90,90,250,30);
+   
+   
+    fill(255,20,30);
+    ellipse(mouseX,mouseY,50,50);
+}
+
+function coin(){
+    
+}
