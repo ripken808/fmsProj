25060d445b39821b180abe58c1e08a84ab409186
Gaben4184 <grodfam4@gmail.com>
2022-11-14 12:50:17 -0700

    coloring and links

---
+++ dev/color.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    
+    fill(20,200,30);
+    rect(90,90,250,30);
+    
+    fill(20,200,30);
+    rect(310,90,30,130);
+    
+    fill(20,200,30);
+    rect(90,200,250,30);
+    
+    fill(20,200,30);
+    rect(90,200,30,130);
+    
+    fill(20,200,30);
+    rect(90,300,250,30);
+    
+    fill(255,20,30);
+    ellipse(mouseX,mouseY,50,50);
+  }
