165fd5879aea7aacb2f34326ebdf9899766627a0
Gaben4184 <grodfam4@gmail.com>
2022-11-18 13:55:12 -0700

    stack

--- dev/stack.js
+++ dev/stack.js
@@ -1,18 +1,39 @@
 function setup() {
-    createCanvas(1200, 1200);
+    createCanvas(1600, 900);
 }
   
 function draw() {
     background(220);
-   
-    fill(20,200,30);
-    ellipse(90,90,250,30);
-   
-   
-    fill(255,20,30);
-    ellipse(mouseX,mouseY,50,50);
+
+    //coin on screen
+    ellipseMode(CENTER);
+    fill(255,223,0);
+    ellipse(1200,460,100,40);
+    line(1149,450,1149,460);
+    line(1250,450,1250,460);
+    ellipseMode(CENTER);
+    fill(255,223,0);
+    ellipse(1200,450,100,40);
+    coin();
 }
+
+
 
 function coin(){
     
+    if (mouseisPressed === true && (mouseX >= 1150 && mouseX <= 1250)){
+        //let dime = false;
+        //if ((mouseX >= 1150 && mouseX <= 1250)){
+        //    dime = true;
+        //}
+        
+
+        //if (dime == true){
+            fill(255,223,0);
+            ellipse(mouseX,mouseY,100,40);
+        //}
+
+    }
+    fill(255,223,0);
+    //ellipse(mousex,(mouseY + 30), 100,40);
 }
