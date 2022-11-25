32749359aec57f8b88e6952c3bd255a50c9639fd
Gaben <grodfam4@gmail.com>
2022-11-25 12:59:04 -0700

    comment to fix

--- dev/stack.js
+++ dev/stack.js
@@ -6,34 +6,47 @@ function draw() {
     background(220);
 
     //coin on screen
-    ellipseMode(CENTER);
+    //ellipseMode(CENTER);
     fill(255,223,0);
     ellipse(1200,460,100,40);
-    line(1149,450,1149,460);
-    line(1250,450,1250,460);
-    ellipseMode(CENTER);
-    fill(255,223,0);
-    ellipse(1200,450,100,40);
-    coin();
+    // ellipseMode(CENTER);
+    // ellipse(1200,450,100,40);
+    // line(1149,450,1149,460);
+    // line(1250,450,1250,460);
+
+    //Table
+    fill(255,248,220);
+    rect(800, 550, 100, 20);
+
+    //coin();
 }
 
 
 
 function coin(){
-    
-    if (mouseisPressed === true && (mouseX >= 1150 && mouseX <= 1250)){
-        //let dime = false;
-        //if ((mouseX >= 1150 && mouseX <= 1250)){
-        //    dime = true;
-        //}
+    // if (mouseisPressed === true && (mouseX >= 1150 && mouseX <= 1250) == true){
+    //     let dime = false;
+    //     if ((mouseX >= 1150 && mouseX <= 1250)){
+    //         dime = true;
+    //     }
         
 
-        //if (dime == true){
-            fill(255,223,0);
-            ellipse(-mouseX,-mouseY,100,40);
-        //}
+    //     if (dime == true){
+    //         x = mouseX;
+    //         y = mouseY;
+           
+    //         ellipseMode(CENTER);
+    //         fill(255,223,0);
+    //         ellipse(x,460,100,40);
+    //         line(x-51,450,x-51,460);
+    //         line(x+50,450,x+50,460);
+    //         ellipseMode(CENTER);
+    //         fill(255,223,0);
+    //         ellipse(x,450,100,40);
+    //       ellipse(x, y, 20, 20);
+    //     }
 
-    }
-    fill(255,223,0);
-    //ellipse(mousex,(mouseY + 30), 100,40);
+    // }
+    // fill(255,223,0);
+    // ellipse(mousex,(mouseY + 30), 100,40);
 }
