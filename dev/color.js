5c2a0bec9006f2d35732b5124dbb0cc85f8284c9
ripken808 <43191093+ripken808@users.noreply.github.com>
2022-11-27 20:51:51 -0700

    Update color.js

--- dev/color.js
+++ dev/color.js
@@ -3,11 +3,28 @@ function setup() {
   background(220);
 }
 function draw() {
+    let mil = millis()/1000;
+    let TIME = 30;
+    let timer = TIME;
+    let over = TIME;
+    if (mil <= TIME){
+        fill(0);
+        textSize(24);
+        text(`Timer: ${round(timer - mil)}`, 1450, 30);
+    }
+    if (mil > TIME){
+        over = 0;
+    }
+    if (over <= 0) {
+        fill(0);
+        textSize(24);
+        text(`Timer: 0`, 1450, 30);
+        fill(220);
 
-  noStroke();
+noStroke();
   fill(220);
 circle(800,450,300);
-
+    }
   if(mouseIsPressed) {
     fill(123,63,0);
   }
